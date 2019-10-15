8452bf23c3413666ceba239a66acb087c03af3c1
Pablo <pablomanuelcoronel@gmail.com>
2019-10-15 16:13:49 -0300

    tarea 1

---
+++ src/main/java/com/codeoftheweb/salvo/Player.java
@@ -0,0 +1,38 @@
+package com.codeoftheweb.salvo;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Player {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private long id;
+    private String userName;
+
+    // constructor por defecto
+    public Player(){}
+
+    // constructor con parametro
+    public Player(String userName){
+        this.userName = userName;
+    }
+
+    // Getters y setters
+    public long getId() {
+        return id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
